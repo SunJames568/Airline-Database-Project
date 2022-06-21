616e7efd584bf81dd2a05b34be739c005b62cd6d
SunJames568 <sunjames568@gmail.com>
2022-06-21 19:35:44 -0400

    Update 4.queries.sql

--- 4.queries.sql
+++ 4.queries.sql
@@ -12,3 +12,15 @@ from customer natural join tickets
 select *
 from airplane
 where airline_name = "Jet Blue";
+
+# Generic view for future flights
+create view future_flight as
+select *
+from flight
+where depart_date_time > CURDATE();
+
+# 1. View Public Info
+#   a. search future flight based on depart airport, arrival airport, and departure date (+ return for round trip)
+select 
+
+#   b. see status based on airline name, flight num, arrival/depart date
