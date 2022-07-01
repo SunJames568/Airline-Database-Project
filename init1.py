0fa258cd09362d157a3c9087a3b87386a208a5d2
SunJames568 <sunjames568@gmail.com>
2022-07-01 11:55:59 -0400

    register/login wip

--- init1.py
+++ init1.py
@@ -24,22 +24,27 @@ def start():
 def login():
 	return render_template('login.html')
 
-#Define route for register
+#Define route for customer register
 @app.route('/register')
 def register():
-	return render_template('register.html')
+	return render_template('registerCustomer.html')
+
+#Define route for staff register
+@app.route('/register')
+def register():
+	return render_template('registerStaff.html')
 
 #Authenticates the login
-@app.route('/loginAuth', methods=['GET', 'POST'])
+@app.route('/loginCustomerAuth', methods=['GET', 'POST'])
 def loginAuth():
 	#grabs information from the forms
-	username = request.form['username']
-	password = request.form['password']
+	username = request.form['customer_email']
+	password = hashlib.md5(request.form['customer_pw'].encode()).hexdigest()
 
 	#cursor used to send queries
 	cursor = conn.cursor()
 	#executes query
-	query = 'SELECT * FROM user WHERE username = %s and password = %s'
+	query = 'SELECT * FROM customer WHERE email = %s and password = %s'
 	cursor.execute(query, (username, password))
 	#stores the results in a variable
 	data = cursor.fetchone()
@@ -50,14 +55,74 @@ def loginAuth():
 		#creates a session for the the user
 		#session is a built in
 		session['username'] = username
-		return redirect(url_for('home'))
+		return redirect(url_for('staffHome'))
+	else:
+		#returns an error message to the html page
+		error = 'Invalid login or email'
+		return render_template('login.html', error=error)
+
+@app.route('/loginStaffAuth', methods=['GET', 'POST'])
+def loginAuth():
+	#grabs information from the forms
+	username = request.form['staff_uname']
+	password = hashlib.md5(request.form['staff_pw']).hexdigest()
+
+	#cursor used to send queries
+	cursor = conn.cursor()
+	#executes query
+	query = 'SELECT * FROM airline_staff WHERE name = %s and password = %s'
+	cursor.execute(query, (username, password))
+	#stores the results in a variable
+	data = cursor.fetchone()
+	#use fetchall() if you are expecting more than 1 data row
+	cursor.close()
+	error = None
+	if(data):
+		#creates a session for the the user
+		#session is a built in
+		session['username'] = username
+		return redirect(url_for('staffHome'))
 	else:
 		#returns an error message to the html page
 		error = 'Invalid login or username'
 		return render_template('login.html', error=error)
 
 #Authenticates the register
-@app.route('/registerAuth', methods=['GET', 'POST'])
+@app.route('/registerCustomerAuth', methods=['GET', 'POST'])
+def registerAuth():
+	#grabs information from the forms
+	email = request.form['email']
+	name = request.form['name']
+	password = hashlib.md5(request.form['password']).hexdigest()
+	building_number = request.form['building_number']
+	street = request.form['street']
+	city = request.form['city']
+	state = request.form['state']
+	phone_number = request.form['phone_number']
+	passport_expiration = request.form['passport_expiration']
+	passport_country = request.form['passport_country']
+	
+	#cursor used to send queries
+	cursor = conn.cursor()
+	#executes query
+	query = 'SELECT * FROM user WHERE email = %s'
+	cursor.execute(query, (email))
+	#stores the results in a variable
+	data = cursor.fetchone()
+	#use fetchall() if you are expecting more than 1 data row
+	error = None
+	if(data):
+		#If the previous query returns data, then user exists
+		error = "This customer already exists"
+		return render_template('register.html', error = error)
+	else:
+		ins = 'INSERT INTO user VALUES(%s, %s)'
+		cursor.execute(ins, (username, password))
+		conn.commit()
+		cursor.close()
+		return render_template('index.html')
+
+@app.route('/registerStaffAuth', methods=['GET', 'POST'])
 def registerAuth():
 	#grabs information from the forms
 	username = request.form['username']
